0f64ce85c80b5f969ba7fb34e1db06a6150d91da
Yw2 Theorycrafter <yw2theorycrafter@gmail.com>
2021-09-13 22:14:42 -0700

    disable headache-inducing fabricatorLight of all Fabricators

---
+++ Logger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace No_Strobe_Fabricator
+{  
+        internal class Logger
+        {
+            internal static void Log(string message)
+            {
+                Console.WriteLine("[No_Strobe_Fabricator] " + message);
+            }
+        }
+}
---
+++ Main.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using QModManager.API.ModLoading;
+using QModManager.API;
+using System.Reflection;
+
+namespace No_Strobe_Fabricator
+{
+    // Your main patching class must have the QModCore attribute (and must be public)
+    [QModCore]
+    public static class Main
+    {
+        // Your patching method must have the QModPatch attribute (and must be public)
+        [QModPatch]
+        public static void Patch()
+        {
+            // Add your patching code here
+            //QModServices.Main.AddCriticalMessage("No Strobe Fabricator loaded.");
+            Harmony harmony = new Harmony("com.yw2theorycrafter.nostrobefabricator");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
