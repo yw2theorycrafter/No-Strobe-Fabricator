975c2229c95c77ad36538d7bd6efcb4929afc5ce
Yw2 Theorycrafter <yw2theorycrafter@gmail.com>
2024-12-14 14:38:14 -0800

    Begin 2.0 upgrade

---
+++ Plugin.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace No_Strobe_Fabricator
+{
+    [BepInPlugin(MyGuid, PluginName, VersionString)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private const string MyGuid = "com.yw2theorycrafter.nostrobefabricator";
+        private const string PluginName = "No Strobe Fabricator";
+        private const string VersionString = "2.0.0";
+
+        public new static ManualLogSource Logger { get; private set; }
+
+        private static Assembly Assembly { get; } = Assembly.GetExecutingAssembly();
+
+        private void Awake()
+        {
+            // plugin startup logic
+            Logger = base.Logger;
+
+            // register harmony patches, if there are any
+            Harmony.CreateAndPatchAll(Assembly, $"{MyGuid}");
+            Logger.LogInfo($"Plugin {MyGuid} is loaded!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# No-Strobe-Fabricator
+Subnautica mod to disable headache inducing flickering light of fabricators
+
+https://www.nexusmods.com/subnautica/mods/843
